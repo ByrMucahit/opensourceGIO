d40e5bf03b04199589947876fbed8aba17bbb126
mucahit bayar <mucahit.bayar@mavidev.com>
2022-10-10 19:31:23 +0300

    Bug: Corst not allowed achieving

---
+++ src/main/java/com/example/opensourcegio/controllers/IssueChallengeController.java
@@ -0,0 +1,25 @@
+package com.example.opensourcegio.controllers;
+
+import com.example.opensourcegio.controllers.requests.UpdateChallengeStatusRequest;
+import com.example.opensourcegio.services.IssueChallengeService;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/challenges")
+public class IssueChallengeController {
+
+    private final IssueChallengeService issueChallengeService;
+
+    public IssueChallengeController(IssueChallengeService issueChallengeService) {
+        this.issueChallengeService = issueChallengeService;
+    }
+
+    @PatchMapping("/{id}")
+    public void updateStatus(@PathVariable("id") Integer id, @RequestBody UpdateChallengeStatusRequest request) {
+        this.issueChallengeService.updateStatus(id,request.getIssueChallengeStatus());
+    }
+}
