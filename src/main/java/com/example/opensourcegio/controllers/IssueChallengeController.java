463491596ecc04fa481dbbdcd405314e097b7251
mucahit bayar <mucahit.bayar@mavidev.com>
2022-10-11 19:58:44 +0300

    Added the service that return all lists.

--- src/main/java/com/example/opensourcegio/controllers/IssueChallengeController.java
+++ src/main/java/com/example/opensourcegio/controllers/IssueChallengeController.java
@@ -1,12 +1,16 @@
 package com.example.opensourcegio.controllers;
 
 import com.example.opensourcegio.controllers.requests.UpdateChallengeStatusRequest;
+import com.example.opensourcegio.models.IssueChallenge;
 import com.example.opensourcegio.services.IssueChallengeService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/challenges")
@@ -21,5 +25,10 @@ public class IssueChallengeController {
     @PatchMapping("/{id}")
     public void updateStatus(@PathVariable("id") Integer id, @RequestBody UpdateChallengeStatusRequest request) {
         this.issueChallengeService.updateStatus(id,request.getStatus());
+    }
+
+    @GetMapping
+    public List<IssueChallenge> list () {
+        return this.issueChallengeService.list();
     }
 }
