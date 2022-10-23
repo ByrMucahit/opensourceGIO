f1393e3713fbed9168a07ec9e29293f16e39d069
mucahit bayar <mucahit.bayar@mavidev.com>
2022-10-23 17:41:14 +0300

    Added github oauth client.

--- src/main/java/com/example/opensourcegio/controllers/GithubAuntController.java
+++ src/main/java/com/example/opensourcegio/controllers/GithubAuntController.java
@@ -1,11 +1,16 @@
 package com.example.opensourcegio.controllers;
 
+import com.example.opensourcegio.controllers.requests.GetGithubAccessTokenRequest;
 import com.example.opensourcegio.services.GithubClient;
+import com.example.opensourcegio.services.models.GithubAccessTokenResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,5 +27,12 @@ public class GithubAuntController {
     public void authorize (HttpServletResponse response) {
         response.setHeader("Location",this.githubClient.getAuthorize());
         response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+    }
+
+    @PostMapping("/access_token")
+    public GithubAccessTokenResponse getAccessToken(@RequestBody GetGithubAccessTokenRequest githubAccessTokenRequest) {
+        return GithubAccessTokenResponse
+                .builder()
+                .accessToken( this.githubClient.getAccessToken(githubAccessTokenRequest.getCode())).build();
     }
 }
--- src/main/java/com/example/opensourcegio/services/GithubClient.java
+++ src/main/java/com/example/opensourcegio/services/GithubClient.java
@@ -2,8 +2,8 @@ package com.example.opensourcegio.services;
 
 import com.example.opensourcegio.config.GithubProperties;
 import com.example.opensourcegio.services.models.GithubAccessTokenRequest;
+import com.example.opensourcegio.services.models.GithubAccessTokenResponse;
 import com.example.opensourcegio.services.models.GithubIssueResponse;
-
 import com.example.opensourcegio.services.models.GithubPullsResponse;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Service
 public class GithubClient {
@@ -26,11 +27,27 @@ public class GithubClient {
         this.githubProperties = githubProperties;
     }
 
+    public String getAccessToken(String code) {
+        GithubAccessTokenRequest accessTokenRequest = GithubAccessTokenRequest.builder()
+                .clientId(this.githubProperties.getClientId())
+                .clientSecret(this.githubProperties.getClientSecret())
+                .code(code).build();
+        HttpEntity<GithubAccessTokenRequest> request = new HttpEntity<>(accessTokenRequest);
+        ResponseEntity<GithubAccessTokenResponse> excahnge =
+                this.restTemplate.exchange(this.githubProperties.getAccessTokenUrl(), HttpMethod.POST, request, GithubAccessTokenResponse.class);
+
+        return Objects.requireNonNull(excahnge.getBody()).getAccessToken();
+    }
+
+    public String getAuthorize() {
+        return String.format("%s?client_id=%s", githubProperties.getAuthorizeUrl(), githubProperties.getClientId());
+    }
+
     public GithubIssueResponse[] listIssues(String owner, String repository, LocalDate since) {
         String issuesUrl = String.format("%s/repos/%s/%s/issues?pulls=false?since=%s",
-                this.githubProperties.getApiUrl(),owner, repository, since.toString());
+                this.githubProperties.getApiUrl(), owner, repository, since.toString());
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "token "+this.githubProperties.getToken());
+        headers.add("Authorization", "token " + this.githubProperties.getToken());
         HttpEntity request = new HttpEntity(headers);
         ResponseEntity<GithubIssueResponse[]> exchange =
                 this.restTemplate.exchange(issuesUrl, HttpMethod.GET, request, GithubIssueResponse[].class);
@@ -40,26 +57,13 @@ public class GithubClient {
 
     public GithubPullsResponse[] listPullRequest(String owner, String repository) {
         String pullRequestsUrl = String.format("%s/repos/%s/%s/pulls?state=closed",
-                this.githubProperties.getApiUrl(),owner, repository);
+                this.githubProperties.getApiUrl(), owner, repository);
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "token "+this.githubProperties.getToken());
+        headers.add("Authorization", "token " + this.githubProperties.getToken());
         HttpEntity request = new HttpEntity(headers);
         ResponseEntity<GithubPullsResponse[]> exchange =
                 this.restTemplate.exchange(pullRequestsUrl, HttpMethod.GET, request, GithubPullsResponse[].class);
 
         return exchange.getBody();
-    }
-
-    public  String getAuthorize() {
-        return String.format("%s?client_id=%s", githubProperties.getAuthorizeUrl(), githubProperties.getClientId());
-    }
-
-    public String getAccessToken(String code) {
-        GithubAccessTokenRequest accessTokenRequest = GithubAccessTokenRequest.builder()
-                .clientId(this.githubProperties.getClientId())
-                .clientSecret(this.githubProperties.getClientSecret())
-                .code(code).build();
-        HttpEntity<GithubAccessTokenRequest> request = new HttpEntity<>(accessTokenRequest);
-        this.restTemplate.exchange(this.githubProperties.getAccessTokenUrl(), HttpMethod.POST, request);
     }
 }
